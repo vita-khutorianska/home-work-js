333b82795ec12f340d3f23f6f3ab0f35ba0a8a2d
Vita Khutorianska <steblvita@gmail.com>
2020-10-25 13:44:18 +0200

    task one +

---
+++ js/task01.js
@@ -0,0 +1,11 @@
+const name = '«Генератор защитного поля»'; // Write code on this line
+console.log(name);
+let price = 1000; // Write code on this line
+console.log(price);
+const firstMessage = `Выбран  ${name}, цена за штуку  ${price} кредитов`;
+console.log(firstMessage);
+ // Write code on this line
+price = 2000;
+console.log(price);
+const secondMessage = `Выбран  ${name}, цена за штуку  ${price} кредитов`; // Write code on this line
+console.log(secondMessage);
