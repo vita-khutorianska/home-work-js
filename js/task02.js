9ebb766a5020e73eab90cb842ee8cb94d6a1805a
Vita Khutorianska <steblvita@gmail.com>
2020-10-25 14:18:28 +0200

    task02done

---
+++ js/task02.js
@@ -0,0 +1,13 @@
+const invoice = 100;
+const stock = 100;
+
+// Write code under this line
+const message = invoice <= stock ? 'Заказ оформлен, с вами свяжется менеджер' : 'На складе недостаточно товаров!';
+
+console.log(message);
+
+//если invoice равен 100
+// то значение message будет равно
+// 'Заказ оформлен, с вами свяжется менеджер'
+
+//если invoice равен 50
