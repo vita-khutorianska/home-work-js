5de14137c79b3378df400c8e648cc5cbbe96cfb7
vita-khutorianska <steblvita@gmail.com>
2020-11-08 15:38:13 +0200

    task 1 one

---
+++ js-3/task3-01.js
@@ -0,0 +1,30 @@
+let message = "";
+const user = {
+  age: 20,
+  hobby: "html",
+  name: "Mango",
+    premium: true,
+};
+user.mood = "happy";
+user['full time'] = true;
+user.hobby = "skydiving";
+user['premium'] = false;
+console.log(user);
+
+//Write code under this line
+
+const keys = Object.keys(user);
+for (const key of keys) {
+  message += `${key} : ${user[key]}\n`;
+}
+// Write code under this line
+
+console.log(message);
+// /*
+// 'age : 20 
+// hobby : skydiving 
+// name : Mango 
+// premium : false 
+// mood : happy 
+// "full time" : true 
+// ' 
